0b2c9076ad730f4dbf002d9ef7e1a36f2139d00d
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2025-01-23 00:31:29 +0300

    First Trial

--- server/models.py
+++ server/models.py
@@ -11,7 +11,21 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+    @validates('name')
+    def validate_author_name(self, key, address):
+        if not address:
+            raise ValueError("Name cannot be empty string")
+        elif address in [Author.name for Author in Author.query.all()]:
+            raise ValueError("Name must be unique")
+        return address
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, address):
+        if not len(address) == 10 or not int(address):
+            raise ValueError("Number must be exactly 10 digits long")
+        return address
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +42,27 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
-
+    @validates('content')
+    def validate_content(self, key, address):
+        if len(address) < 250:
+            raise ValueError("Content must be at least 250 characters long")
+        return address
+    
+    @validates('summary')
+    def validate_summary(self, key, address):
+        if len(address) > 250:
+            raise ValueError("Summary must be less than 250 characters")
+        return address
+    
+    @validates('category')
+    def validate_category(self, key, address):
+        if address not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Invalid Category")
+        
+    @validates('title')
+    def validate_title(self, key, address):
+        if not address or [el in address for el in ["Won't Believe", "Secret", "Top", "Guess"]].count(True) == 0:
+            raise ValueError("Not click-baity enough!")
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
